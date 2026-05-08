0d5158f0760f3768321b70a084efe166c758228e
Rahat Husain khan <123046685+Rahat-coder971@users.noreply.github.com>
2026-05-08 18:46:43 +0530

    Update server response message

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.write(`server is running on port 3000`);
+  res.write(`server is running on port 3000 and is updated`);
   res.end();
 });
 
