423348d4dcfab5f6f109fb824c10b643b241d80e
Rahat Husain khan <123046685+Rahat-coder971@users.noreply.github.com>
2026-05-09 14:00:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.write(`this is updated resposnse`);
+  res.write(`this is updated resposnse and this is upadted code`);
   res.end();
 });
 
