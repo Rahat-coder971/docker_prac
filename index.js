e407109535ca8374eed08058db9a9aa011421885
Rahat Husain khan <123046685+Rahat-coder971@users.noreply.github.com>
2026-05-09 12:55:48 +0530

    Update server response message with version info

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.write(`server is running on port 3000 and is updated`);
+  res.write(`server is running on port 3000 and is updated and this imge is the latest v2`);
   res.end();
 });
 
