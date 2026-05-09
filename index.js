1ffa5fe3d1ef1dda695e75091f5f1a9cd5a04be1
Rahat Husain khan <123046685+Rahat-coder971@users.noreply.github.com>
2026-05-09 13:06:31 +0530

    Update server response message

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.write(`server is running on port 3000 and is updated and this imge is the latest v2`);
+  res.write(`this is updated resposnse`);
   res.end();
 });
 
