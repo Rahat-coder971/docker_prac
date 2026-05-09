1cf2a5d5ff873fe68679d425e4c393b6b47820a0
Rahat Husain khan <123046685+Rahat-coder971@users.noreply.github.com>
2026-05-09 15:00:07 +0530

    Update server response message in index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.write(`this is updated resposnse and this is upadted code`);
+  res.write(`this is done from cicd`);
   res.end();
 });
 
